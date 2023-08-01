728a10535c28501314f15b07cd1bf2672f581c7b
Zaibonisha <zsmohamed94@gmail.com>
2023-08-01 21:16:54 +0200

    Changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Card, CardContent, Typography, CardMedia, Grid } from '@mui/material';
+import { Card, CardContent, Typography, CardMedia, Grid, Button } from '@mui/material';
 
 const About = () => {
+  const imageStyle = {
+    width: '20vw',
+    height: '65vh',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    marginBottom: '10px',
+    position: 'relative',
+  };
+
+  
+
+  
+  const buttonStyle = {
+    position: 'absolute',
+    bottom: '10px',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    backgroundColor: '#efca08',
+    
+  };
   return (
     <div>
 
       <Grid container spacing={0}>
-        <Grid item xs={12} sm={6} style={{ width: '50vw' }}>
-          <Card style={{ marginBottom: 0, width: '50vw', height: '100vh' }}>
+        <Grid item xs={12} sm={6} style={{ width: '50vw'}}>
+          <Card style={{ marginBottom: 0, width: '50vw', height: '100vh',backgroundColor: '#e8dab2' }}>
             <CardContent>
               <Typography variant="h1" component="h2">
               <img src="ABOUTheading.png" alt="About" width="100%" height="50%"/>
@@ -20,7 +40,7 @@ const About = () => {
         </Grid>
 
         <Grid item xs={12} sm={6} style={{ marginLeft: '0' }}>
-          <Card style={{ marginBottom: 0, width: '50vw', height: '100vh' }}>
+          <Card style={{ marginBottom: '30px', width: '50vw', height: '100vh' }}>
             <CardMedia
               component="img"
               alt="Picture"
@@ -31,7 +51,142 @@ const About = () => {
           </Card>
         </Grid>
       </Grid>
+
+      <div style={{marginBottom: '10px'}}>
+      <img src="OUR MVP.png" alt="About" width="70%" height="50%"/>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item>
+          <div style={{ ...imageStyle, backgroundImage: 'url("Rectangle6.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Hun ger.png" alt="Hunger" width="50%" height="50%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '20px' }}>
+            <img src="Africa.png" alt="Causes" width="50%" height="50%" />
+          </div>
+            <Button variant="contained" style={buttonStyle}>
+              Button 1
+            </Button>
+          </div>
+        </Grid>
+        <Grid item>
+          <div style={{...imageStyle,backgroundImage: 'url("Rectangle7.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Shelter refugees.png" alt="Shelter refugees" width="90%" height="90%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '20px' }}>
+            <img src="Ukraine.png" alt="Causes" width="50%" height="50%" />
+          </div>
+            <Button variant="contained" style={buttonStyle}>
+              Button 2
+            </Button>
+          </div>
+        </Grid>
+        <Grid item>
+          <div style={{ ...imageStyle, backgroundImage: 'url("Rectangle8.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Edu cate.png" alt="Educate" width="50%" height="50%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '50px' }}>
+            <img src="Africa.png" alt="Causes" width="50%" height="50%" />
+          </div>
+          
+            <Button variant="contained" style={buttonStyle}>
+              Button 3
+            </Button>
+          </div>
+        </Grid>
+        
+        </Grid>
+      
+      </div>
+      <div style={{ marginTop: '50px', display: 'flex', justifyContent: 'center' }}>
+        <div style={{ marginRight: '100px' }}>
+          {/* Third Image */}
+          <img
+            src={`${process.env.PUBLIC_URL}/Line 1.png`}  // Replace 'third_image_url.png' with the actual URL of the third image
+            alt="Third Image"
+            style={{ width: '10vh', height: '80vh' }}
+          />
+          {/* Fourth Image */}
+          <img
+            src={`${process.env.PUBLIC_URL}/mousescrolldown.png`}  // Replace 'fourth_image_url.png' with the actual URL of the fourth image
+            alt="Fourth Image"
+            style={{ width: '10vw', height: 'auto', marginBottom: '10vw' }}
+          />
+          
+        </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/meet-the-team.png`}  // Replace 'second_image_url.png' with the actual URL of the second image
+          alt="Second Image"
+          style={{ width: '75%', height: '80vh',  }}
+         
+        />
+      </div>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item>
+          <div style={{ ...imageStyle, backgroundImage: 'url("Rectangle6.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Hun ger.png" alt="Hunger" width="50%" height="50%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '20px' }}>
+            <img src="Africa.png" alt="Causes" width="50%" height="50%" />
+          </div>
+            <Button variant="contained" style={buttonStyle}>
+              Button 1
+            </Button>
+          </div>
+        </Grid>
+        <Grid item>
+          <div style={{...imageStyle,backgroundImage: 'url("Rectangle7.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Shelter refugees.png" alt="Shelter refugees" width="90%" height="90%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '20px' }}>
+            <img src="Ukraine.png" alt="Causes" width="50%" height="50%" />
+          </div>
+            <Button variant="contained" style={buttonStyle}>
+              Button 2
+            </Button>
+          </div>
+        </Grid>
+        <Grid item>
+          <div style={{ ...imageStyle, backgroundImage: 'url("Rectangle8.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Edu cate.png" alt="Educate" width="50%" height="50%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '50px' }}>
+            <img src="Africa.png" alt="Causes" width="50%" height="50%" />
+          </div>
+          
+            <Button variant="contained" style={buttonStyle}>
+              Button 3
+            </Button>
+          </div>
+        </Grid>
+        <Grid item>
+          <div style={{ ...imageStyle, backgroundImage: 'url("Rectangle6.png")' }}>
+            
+          <div style={{ textAlign: 'topleft', marginBottom: '20px' }}>
+            <img src="Hun ger.png" alt="Hunger" width="50%" height="50%" />
+          </div>
+          <div style={{ textAlign: 'topleft', marginTop: '20px' }}>
+            <img src="Africa.png" alt="Causes" width="50%" height="50%" />
+          </div>
+            <Button variant="contained" style={buttonStyle}>
+              Button 1
+            </Button>
+          </div>
+        </Grid>
+        </Grid>
+      
     </div>
+    
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import { Card as MUICard, CardContent, Button, Typography, Grid } from '@mui/material';
+
+const Card = ({ title, description, image, buttonLabel, buttonLink }) => {
+  const buttonStyle = {
+    color: 'white',
+    textDecoration: 'none',
+    cursor: 'pointer',
+    backgroundColor: '#fca311', // Replace with your desired background color
+    padding: '8px 16px', // Add padding for better appearance
+    borderRadius: '4px', // Add border radius for rounded corners
+    width: '20vw',
+    marginTop: '50px'
+  };
+
+  return (
+    <MUICard>
+      <CardContent>
+        {image && <img src={image} alt={title} style={{ width: '20vw', height: '30vh' }} />}
+        <Typography variant="h5">{title}</Typography>
+        <Typography variant="body1">{description}</Typography>
+        <Button component="a" href={buttonLink} style={buttonStyle}>
+          {buttonLabel}
+        </Button>
+      </CardContent>
+    </MUICard>
+  );
+};
 
 const Home = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -9,7 +36,7 @@ const Home = () => {
     backgroundImage: `url('${images[currentImage]}')`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    height: '90vh',
+    height: '80vh',
     position: 'relative',
   };
 
@@ -29,11 +56,13 @@ const Home = () => {
 
   return (
     <div>
+      
+
       <div style={backgroundImageStyle} onClick={() => switchToImage((currentImage + 1) % images.length)}>
-        <h1 style={{ color: '#5a189a', position: 'absolute', top: 0, left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', width: '100%' }}>
+        <h1 style={{ color: '#003459', position: 'absolute', top: 0, left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', width: '100%' }}>
           The change starts now!
         </h1>
-        <h2 style={{ color: '#5a189a', position: 'absolute', top: '10%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', width: '100%' }}>
+        <h2 style={{ color: '#003459', position: 'absolute', top: '10%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', width: '100%' }}>
           Lend a helping hand to those in need
         </h2>
         <div style={{ position: 'absolute', bottom: '5%', left: '50%', transform: 'translateX(-50%)', display: 'flex', gap: '10px' }}>
@@ -51,6 +80,63 @@ const Home = () => {
             />
           ))}
         </div>
+      </div>
+      {/* Second Image */}
+      <div style={{ marginTop: '30px', display: 'flex', justifyContent: 'center' }}>
+        <div style={{ marginRight: '100px' }}>
+          {/* Third Image */}
+          <img
+            src={`${process.env.PUBLIC_URL}/Line 1.png`}  // Replace 'third_image_url.png' with the actual URL of the third image
+            alt="Third Image"
+            style={{ width: '10vh', height: '80vh' }}
+          />
+          {/* Fourth Image */}
+          <img
+            src={`${process.env.PUBLIC_URL}/mousescrolldown.png`}  // Replace 'fourth_image_url.png' with the actual URL of the fourth image
+            alt="Fourth Image"
+            style={{ width: '10vw', height: 'auto', marginBottom: '10vw' }}
+          />
+          
+        </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/howdowe.png`}  // Replace 'second_image_url.png' with the actual URL of the second image
+          alt="Second Image"
+          style={{ width: '75%', height: '80vh' }}
+         
+        />
+      </div>
+    {/* Card Components */}
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item>
+            <Card
+              title="Mision"
+              description="This is the description for Card 1."
+              image="mision1.png"
+              buttonLabel="Join the mission"
+              buttonLink="/volunteers"
+            />
+            
+          </Grid>
+          <Grid item>
+            <Card
+              title="Vision"
+              description="This is the description for Card 2."
+              image="vision1.png"
+              buttonLabel="Help us today"
+              buttonLink="/donations"
+            />
+          </Grid>
+          <Grid item>
+            <Card
+              title="Passion"
+              description="This is the description for Card 3."
+              image="goals.png"
+              buttonLabel="Reach the goals with us"
+              buttonLink="/causes"
+            />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
