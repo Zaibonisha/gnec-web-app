ea4fd43d727fcf6f0db4218e0d34c7fb69db82be
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-08-05 06:37:05 +0100

    Add JoinUs component

---
+++ src/components/volunteer/JoinUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const JoinUs = () => {
+  return (
+    <div className="p-10">
+      <div className="flex flex-row gap-10 "> 
+        <p className="w-[200px] text-[35px] text-[#FFC107]">JOIN US TODAY</p>
+        <p className="w-[400px]">Lorem ipsum sit dolorLorem ipsum sit dolorLorem ipsum sit dolorLorem ipsum sit dolorLorem ipsum sit dolorLorem ipsum sit dolorLorem ipsum sit dolorLorem ipsum sit dolorLorem.</p>
+      </div>
+    </div>
+  )
+}
+
+export default JoinUs
--- src/components/volunteer/index.jsx
+++ src/components/volunteer/index.jsx
@@ -3,6 +3,7 @@ import HeaderCarousal from "../common/HeaderCarousal";
 import PageTitle from "../common/PageTitle";
 import "../../assets/style/volunteer.css";
 import Cards from "./Cards";
+import JoinUs from "./JoinUs";
 
 const Volunteer = () => {
   return (
@@ -13,6 +14,7 @@ const Volunteer = () => {
         className="font-bold text-[100px] my-10 text-center text"
       />
       <Cards />
+      <JoinUs />
     </>
   );
 };
