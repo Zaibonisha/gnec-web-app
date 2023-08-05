2fedf68b21d3fd9c71db53bc64ee71156ccbd4df
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-08-05 05:13:41 +0100

    Add volunteer page title

--- src/components/volunteer/index.jsx
+++ src/components/volunteer/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HeaderCarousal from '../common/HeaderCarousal'
 import PageTitle from '../common/PageTitle'
+import '../../assets/style/volunteer.css'
 
 const Volunteer = () => {
   return (
     <>
     <HeaderCarousal />
-    <PageTitle title="VOLUNTEER" className="font-bold text-[35px] text-center" />
+    <PageTitle title="VOLUNTEER" className="font-bold text-[100px] my-10 text-center text" />
     </>
   )
 }
