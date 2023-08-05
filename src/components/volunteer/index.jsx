8a9119fda6c439d2bfa56bb1249062649a7ef8d1
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-08-05 04:53:27 +0100

    Add tailwindCSS

--- src/components/volunteer/index.jsx
+++ src/components/volunteer/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeaderCarousal from '../common/HeaderCarousal'
+import PageTitle from '../common/PageTitle'
 
 const Volunteer = () => {
   return (
     <>
     <HeaderCarousal />
+    <PageTitle title="VOLUNTEER" className="font-bold text-[35px] text-center" />
     </>
   )
 }
