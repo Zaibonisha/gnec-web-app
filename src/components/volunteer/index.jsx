9e6aa6d354b5b62afc75101b726edbe4fb22f50c
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-08-05 06:28:17 +0100

    Add card component

--- src/components/volunteer/index.jsx
+++ src/components/volunteer/index.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import HeaderCarousal from '../common/HeaderCarousal'
-import PageTitle from '../common/PageTitle'
-import '../../assets/style/volunteer.css'
+import React from "react";
+import HeaderCarousal from "../common/HeaderCarousal";
+import PageTitle from "../common/PageTitle";
+import "../../assets/style/volunteer.css";
+import Cards from "./Cards";
 
 const Volunteer = () => {
   return (
     <>
-    <HeaderCarousal />
-    <PageTitle title="VOLUNTEER" className="font-bold text-[100px] my-10 text-center text" />
+      <HeaderCarousal />
+      <PageTitle
+        title="VOLUNTEER"
+        className="font-bold text-[100px] my-10 text-center text"
+      />
+      <Cards />
     </>
-  )
-}
+  );
+};
 
-export default Volunteer
+export default Volunteer;
